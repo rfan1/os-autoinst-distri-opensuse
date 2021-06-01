ab350aa174c052acebacd46227e4dbdd1da6f263
rfan1 <richard.fan@suse.com>
2021-06-01 04:45:31 -0400

    openssl rehash

--- tests/security/grub_auth/grub_authorization.pm
+++ tests/security/grub_auth/grub_authorization.pm
@@ -31,82 +31,13 @@ use strict;
 use warnings;
 use testapi;
 use base 'consoletest';
-
-my $sup_user     = 'admin';
-my $sup_passwd   = 'pw_admin';
-my $maint_user   = 'maintainer';
-my $maint_passwd = 'pw_maintainer';
-my $test_user    = 'tester';
-my $test_passwd  = 'pw_test';
-
-sub grub_auth_oper {
-    my $para = shift;
-    enter_cmd("reboot");
-    if ($para eq "operator") {
-        assert_screen("grub_auth_boot_menu_entry", timeout => 90);
-        send_key("ret");
-        assert_screen("grub_auth_super_user_login");
-        enter_cmd("$sup_user");
-        enter_cmd("$sup_passwd");
-    }
-    elsif ($para eq "maintainer") {
-        assert_screen("grub_auth_boot_menu_entry_maintainer", timeout => 90);
-        send_key("down");
-        send_key("ret");
-        assert_screen("grub_auth_maintain_user_login");
-        enter_cmd("$maint_user");
-        enter_cmd("$maint_passwd");
-    }
-    elsif ($para eq "grub_edit_mode") {
-        assert_screen("grub_auth_boot_menu_entry", timeout => 90);
-        send_key("e");
-        assert_screen("grub_auth_super_user_login");
-        enter_cmd("$sup_user");
-        enter_cmd("$sup_passwd");
-        assert_screen("grub_auth_edit_mode");
-        send_key("ctrl-x");
-    }
-    elsif ($para eq "wrong_user_passwd") {
-        assert_screen("grub_auth_boot_menu_entry", timeout => 90);
-        send_key("ret");
-        assert_screen("grub_auth_super_user_login");
-        enter_cmd("$test_user");
-        enter_cmd("$test_passwd");
-        assert_screen("grub_auth_boot_menu_entry");
-        send_key("ret");
-        assert_screen("grub_auth_super_user_login");
-        enter_cmd("$sup_user");
-        enter_cmd("$test_passwd");
-        assert_screen("grub_auth_boot_menu_entry");
-    }
-}
+use utils 'zypper_call';
 
 sub run {
-    select_console("root-console");
-
-    # Check disk name, partition number and fs_type for root file system,
-    # then create a new custom grub config file based on the users/passwords we definded
-    assert_script_run "wget --quiet " . data_url("grub_auth/create_custom_grub.sh");
-    assert_script_run "wget --quiet " . data_url("grub_auth/grub_passwd.sh");
-    assert_script_run("bash grub_passwd.sh $sup_passwd > /tmp/sup_passwd_hash");
-    assert_script_run("bash grub_passwd.sh $maint_passwd > /tmp/maint_passwd_hash");
-    assert_script_run("bash create_custom_grub.sh $sup_user $sup_passwd $maint_user $maint_passwd");
-    assert_script_run("rm -rf /tmp/sup_passwd_hash");
-    assert_script_run("rm -rf /tmp/maint_passwd_hash");
-
-    # Make sure both super user and maintainer can access the grub
-    # the sure user can edit the existing boot menu entries
-    foreach my $i ("operator", "maintainer", "grub_edit_mode") {
-        grub_auth_oper($i);
-        assert_screen("linux-login", timeout => 90);
-        reset_consoles;
-        select_console("root-console");
-    }
-
-    # Make sure not authorized user can not access the grub,
-    # access will fail if we type the wrong password as well,
-    # and the OS will switch back to boot menu entry
-    grub_auth_oper("wrong_user_passwd");
+    my $self = shift;
+    $self->select_serial_terminal;
+    zypper_call("in 389-ds sssd sssd-ldap 'openssl(cli)'");
+    assert_script_run("/usr/bin/openssl rehash /etc/openldap");
 }
 
 sub test_flags {
