16c75166cbcab942b0796587ea488633d61ec02a
rfan1 <richard.fan@suse.com>
2022-05-30 04:57:41 +0200

    Fix apache2_changehat on SLE15<SP4
    Due to bsc#1191684, the case apache2_changehat may fail
    on sles15sp4- version as well, so we need add some workaround
    here for QEM tests. will remove the workaround once the issue
    is fixed.

--- tests/security/apparmor_profile/apache2_changehat.pm
+++ tests/security/apparmor_profile/apache2_changehat.pm
@@ -36,7 +36,7 @@
 #   $audit_log" and fail test.
 # - upload /var/log/apache2/error_log and audit.log
 # Maintainer: llzhao <llzhao@suse.com>
-# Tags: poo#48773, tc#1695946, poo#111036
+# Tags: poo#48773, tc#1695946, poo#111036, poo#111680
 
 
 use base apparmortest;
@@ -153,8 +153,13 @@ sub run {
     my @check_list = ('file_receive', 'open', 'signal', 'mknod');
     foreach my $check_point (@check_list) {
         if ($script_output =~ m/type=AVC .*apparmor=.*DENIED.* operation=.*$check_point.* profile=.*httpd-prefork.*/sx) {
-            record_info("ERROR", "There are denied $check_point records found in $audit_log", result => 'fail');
-            $self->result('fail');
+            if (!is_sle('<=15-SP3')) {
+                record_info("ERROR", "There are denied $check_point records found in $audit_log", result => 'fail');
+                $self->result('fail');
+            }
+            else {
+                record_soft_failure('bsc#1191684', 'Apparmor profile test case "apache2_changehat" found some "DENIED" audit records');
+            }
         }
     }
 
