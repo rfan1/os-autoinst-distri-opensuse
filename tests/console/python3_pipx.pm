72247817771632663bd84747b429d9178120ff9c
rfan1 <richard.fan@suse.com>
2024-10-31 20:22:34 -0400

    Python313 no debs

--- tests/console/python3_pipx.pm
+++ tests/console/python3_pipx.pm
@@ -43,7 +43,7 @@ sub run_tests ($python3_spec_release) {
         record_info("Skip!", "either $python3_spec_release-pipx or $python3_spec_release-wheel doesn't exist");
         return;
     }
-    zypper_call("in $python3_spec_release-pipx $python3_spec_release-wheel");
+    zypper_call("in $python3_spec_release-pipx $python3_spec_release-setuptools $python3_spec_release-wheel");
     my $python_binary = get_python3_binary($python3_spec_release);
     my $version_number = (split("python", $python_binary))[1];
     script_run("$python_binary setup.py bdist_wheel");
