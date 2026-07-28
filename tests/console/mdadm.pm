13ddb13678a09a87431f375302e23ce655db9fa1
rfan1 <richard.fan@suse.com>
2026-07-28 04:39:20 -0400

    MDADM

--- tests/console/mdadm.pm
+++ tests/console/mdadm.pm
@@ -16,8 +16,10 @@ use Utils::Logging 'save_and_upload_log';
 use serial_terminal 'select_serial_terminal';
 use package_utils 'install_package';
 use version_utils 'is_sle';
+use power_action_utils;
 
 sub run {
+    my $self = shift;
     select_serial_terminal;
 
     install_package('mdadm expect', trup_reboot => 1);
@@ -25,6 +27,11 @@ sub run {
     record_info("mdadm build", script_output("rpm -q --qf '%{version}-%{release}' mdadm"));
 
     assert_script_run 'wget ' . data_url('qam/mdadm.sh');
+    assert_script_run 'wget http://openqa.suse.de/assets/repo/kernel-default-6.12.0-160099.294.1.g2573cf4.x86_64.rpm';
+    assert_script_run 'rpm -Uvh --force --nodeps kernel-default-6.12.0-160099.294.1.g2573cf4.x86_64.rpm';
+    power_action('reboot', textmode => 1);
+    $self->wait_boot(ready_time => 600, bootloader_time => get_var('BOOTLOADER_TIMEOUT', 300));
+    select_serial_terminal;
 
     my $timeout = 360;
     if (is_sle('<15')) {
